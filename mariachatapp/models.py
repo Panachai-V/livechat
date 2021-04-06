a506476322fed8030e544e1e30e80fcdad2dd8c4
Nutnicha-S <email>
2021-04-06 21:07:33 +0700

    Create model mariachatapp

--- mariachatapp/models.py
+++ mariachatapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    e-mail = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class Group(models.Model):
+    groupname = models.CharField(max_length=30)
+    groupID = models.IntegerField(max_length=10)
+    username = models.ManyToManyField(User) #ForiengKey IS_IN relation
+
+    def __str__(self):
+        return self.groupID
+
+class MessageLog(models.Model):
+    msgtext = models.CharField(max_length=1000)
+    msgID = models.IntegerField(max_length=10)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    chatID = models.ManyToManyField(User) #ForiengKey PRIVATE_CHAT
+    groupID = models.ManyToManyField(Group) #ForiengKey GROUP_CHAT
+
+    def __str__(self):
+        return self.msgID
